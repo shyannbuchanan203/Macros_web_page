cde94daa7fa076a341d5d038e9f0c9af45026932
shyannbuchanan203 <167809647+shyannbuchanan203@users.noreply.github.com>
2024-04-24 08:09:43 -0700

    Add files via upload

---
+++ макрос вебстраниц.py
@@ -0,0 +1,7 @@
+Sub WhiH()
+ Dim link As Variant
+   link="http://название сайта1"
+   CreateObject("WScript.Shell").Run link
+   link="http://название сайта2"
+   CreateObject("WScript.Shell").Run link
+End Sub
